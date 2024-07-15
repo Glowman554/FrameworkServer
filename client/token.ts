c56860bb7047eb63aca4ad176d0c4ebf32b3b39d
Glowman554 <glowman434@gmail.com>
2024-07-15 23:29:45 +0200

    added request caching / deduplication

--- client/token.ts
+++ client/token.ts
@@ -3,16 +3,20 @@ import { trpc } from "../server/trpc/client.ts";
 import { QueryState, useQuery, withQuery } from "./helper.ts";
 
 export function useToken(q: QueryState) {
-    const query = useQuery(async () => {
-        const token = localStorage.getItem("token");
-        if (token) {
-            const ok = await trpc.users.test.query(token);
-            if (ok) {
-                return token;
+    const query = useQuery(
+        async () => {
+            const token = localStorage.getItem("token");
+            if (token) {
+                const ok = await trpc.users.test.query(token);
+                if (ok) {
+                    return token;
+                }
             }
-        }
-        return undefined;
-    }, q);
+            return undefined;
+        },
+        q,
+        "token",
+    );
 
     return query;
 }
@@ -24,12 +28,12 @@ export function useIsAdmin(
     const [isAdmin, setIsAdmin] = useState(false);
 
     useEffect(() => {
-        console.log("here");
         if (token) {
             withQuery(
                 () => trpc.users.isAdmin.query(token),
                 q,
                 setIsAdmin,
+                "isAdmin",
             );
         }
     }, [token]);
